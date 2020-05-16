a8868071f1b41333f26c3cb84ee3430546a9c261
“Maeva <maeva.fincker@gmail.com>
2020-05-16 19:12:40 -0400

    fix linting issues

--- covid-19-support/src/components/AboutUs.vue
+++ covid-19-support/src/components/AboutUs.vue
@@ -1,40 +1,48 @@
 <template>
-<!-- About Us -->
-<div id="#about-us">
-  <h1>{{ $t('about.about-us.header') }}</h1>
-  <i18n path="about.about-us.p1.text" tag="p">
-  <a href="https://www.google.com/maps/d/u/0/viewer?mid=1xfWfgbjULao-rlKJFfWT07hR-StV4Zx9&ll=37.46071505887045%2C-121.93017180578084&z=11">{{ $t('about.about-us.p1.link') }}</a>
-  </i18n>
-  <i18n path="about.about-us.p2.text" tag="p">
-  <a href="https://nccovidsupport.org/">NC Covid Support</a>
-  </i18n>
-  <i18n path="about.about-us.p3.text" tag="p">
-  <a href="https://github.com/dcl-covid-19/mega-map-dev">{{ $t('about.about-us.p3.link') }}</a>
-  </i18n>
-  <!-- Info sources -->
-  <h1>{{ $t('about.info-sources.header') }}</h1>
-  <p>{{ $t('about.info-sources.p1')}}</p>
-  <ul>
-    <li><a href="https://www.ebt.ca.gov/locator/index.html">{{ $t('about.info-source.link1')}}</a></li>
-    <li><a href="https://www.wicstorelocator.com/address_geo.php">{{ $t('about.info-source.link2')}}</a></li>
-    <li><a href="https://www.shfb.org/">{{ $t('about.info-source.link3')}}</a></li>
-    <li><a href="http://med.stanford.edu/pedsadvocacy.html">{{ $t('about.info-source.link4')}}</a></li>
-  </ul>
-  <p>{{ $t('about.info-sources.p2')}}</p>
-  <!-- Contact Us -->
-  <h1>{{ $t('about.contact-us.header') }}</h1>
-  <i18n path="about.contact-us.p1.text" tag="p">
-  <a href="mailto:hi@bayareacommunity.org">hi@bayareacommunity.org</a>.
-  </i18n>
-  <!-- Disclaimer -->
-  <h1>{{ $t('about.disclaimer.header') }}</h1>
-  <p>{{ $t('about.disclaimer.p')}}</p>
-</div>
+  <!-- About Us -->
+  <div id="#about-us">
+    <h1>{{ $t('about.about-us.header') }}</h1>
+    <i18n path="about.about-us.p1.text" tag="p">
+      <a
+        href="https://www.google.com/maps/d/u/0/viewer?mid=1xfWfgbjULao-rlKJFfWT07hR-StV4Zx9&ll=37.46071505887045%2C-121.93017180578084&z=11"
+        >{{ $t('about.about-us.p1.link') }}</a
+      >
+    </i18n>
+    <i18n path="about.about-us.p2.text" tag="p">
+      <a href="https://nccovidsupport.org/">NC Covid Support</a>
+    </i18n>
+    <i18n path="about.about-us.p3.text" tag="p">
+      <a href="https://github.com/dcl-covid-19/mega-map-dev">{{ $t('about.about-us.p3.link') }}</a>
+    </i18n>
+    <!-- Info sources -->
+    <h1>{{ $t('about.info-sources.header') }}</h1>
+    <p>{{ $t('about.info-sources.p1') }}</p>
+    <ul>
+      <li>
+        <a href="https://www.ebt.ca.gov/locator/index.html">{{ $t('about.info-source.link1') }}</a>
+      </li>
+      <li>
+        <a href="https://www.wicstorelocator.com/address_geo.php">{{ $t('about.info-source.link2') }}</a>
+      </li>
+      <li>
+        <a href="https://www.shfb.org/">{{ $t('about.info-source.link3') }}</a>
+      </li>
+      <li>
+        <a href="http://med.stanford.edu/pedsadvocacy.html">{{ $t('about.info-source.link4') }}</a>
+      </li>
+    </ul>
+    <p>{{ $t('about.info-sources.p2') }}</p>
+    <!-- Contact Us -->
+    <h1>{{ $t('about.contact-us.header') }}</h1>
+    <i18n path="about.contact-us.p1.text" tag="p"> <a href="mailto:hi@bayareacommunity.org">hi@bayareacommunity.org</a>. </i18n>
+    <!-- Disclaimer -->
+    <h1>{{ $t('about.disclaimer.header') }}</h1>
+    <p>{{ $t('about.disclaimer.p') }}</p>
+  </div>
 </template>
 <script>
 export default {
   name: 'about-us'
 }
 </script>
-<style lang="scss">
-</style>
+<style lang="scss"></style>
