8e603ccb42cc471868dc2638cffca4a50d4b1321
“Maeva <maeva.fincker@gmail.com>
2020-06-11 12:32:28 -0400

    refactor to use resource definition objects

---
+++ covid-19-support/src/resources/filters.js
@@ -0,0 +1,26 @@
+// Boolean filters
+export const booleanFilters = [
+  'special_hours',
+  'farmers_market',
+  'wic',
+  'snap',
+  'call_in_advance',
+  'seniors',
+  'children',
+  'public',
+  'telehealth',
+  'in_person',
+  'legal_criminal',
+  'legal_domviolence',
+  'legal_worker_protection',
+  'legal_healthcare',
+  'legal_housing',
+  'legal_immigration'
+]
+
+// Combination of boolean filters
+export const complexFilters = [{ name: 'safe_pick_up', columns: ['curbside_pickup', 'delivery'], combine: ([a, b]) => a || b }]
+
+// Day filters
+export const dayFilters = ['sun', 'mon', 'tues', 'wed', 'thr', 'fri', 'sat'].map((attr) => `${attr}`)
+export const seniorDayFilters = ['sun', 'mon', 'tues', 'wed', 'thr', 'fri', 'sat'].map((attr) => `sp_${attr}`)
