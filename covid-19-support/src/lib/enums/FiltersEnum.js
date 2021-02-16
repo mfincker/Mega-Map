8926b2d50312636477992710b3a5c2a159305e1e
Carlos Alba <soradev4@gmail.com>
2021-02-15 17:54:41 -0800

    FiltersEnum.js

---
+++ covid-19-support/src/lib/enums/FiltersEnum.js
@@ -0,0 +1,10 @@
+export default Object.freeze({
+  SNAP_WIC_RETAILER: 'snap_wic_retailer',
+  SCHOOL_MEAL: 'school_meal',
+  FREE_FOOD: 'free_food',
+  MENTAL_HEALTH: 'mental_health',
+  LEGAL_ASSISTANCE: 'legal_assistance',
+  HEALTH: 'health',
+  DV_RESOURCES: 'dv_resources',
+  CASH_ASSISTANCE: 'cash_assistance'
+})
